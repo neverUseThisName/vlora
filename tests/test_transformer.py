f76b8c4f4a15c841e257f945e7de6f0491622059
neverusethisname <13056660723@163.com>
2024-04-16 12:13:51 +0800

    1) change vlorafy to use metaclass. 2) add test for model saving and loading.

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1,14 +1,20 @@
+import os
+
 import torch
 
 
-def test_transformer():
+def test_forward():
     from vlora.models import VisionTransformerVLoRA
 
     vit = VisionTransformerVLoRA()
     x = torch.randn((1, 3, 224, 224))
     y = vit(x)
-    print(y.shape)
 
 
-if __name__ == "__main__":
-    test_transformer()
+def test_save_and_load():
+    from vlora.models import VisionTransformerVLoRA
+
+    vit = VisionTransformerVLoRA()
+    torch.save(vit.state_dict(), "vlora_vit.pt")
+    vit.load_state_dict(torch.load("vlora_vit.pt"))
+    os.remove("vlora_vit.pt")
