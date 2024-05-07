17fbe05796adbd2764ca9a5d7e15535dfa750a41
neverusethisname <13056660723@163.com>
2024-05-08 00:49:21 +0800

    Add model param count test

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -18,3 +18,19 @@ def test_save_and_load():
     torch.save(vit.state_dict(), "vlora_vit.pt")
     vit.load_state_dict(torch.load("vlora_vit.pt"))
     os.remove("vlora_vit.pt")
+
+
+def test_vlora_params_count():
+    from vlora.models import VisionTransformerVLoRA
+    from timm.models import VisionTransformer
+    
+    def count_params(model):
+        n = 0
+        for param in model.parameters():
+            n += param.nelement()
+        return n
+
+    vlora_vit = VisionTransformerVLoRA()
+    vit = VisionTransformer()
+    assert count_params(vlora_vit) * 2 < count_params(vit)
+
